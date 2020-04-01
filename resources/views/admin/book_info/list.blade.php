b49e84e93d165ff3bddeca6e4d4681b22ab06b84
Le Minh Quan <leminhquan59@gmail.com>
2020-04-01 12:26:52 +0700

    authentication

--- resources/views/admin/book_info/list.blade.php
+++ resources/views/admin/book_info/list.blade.php
@@ -34,7 +34,7 @@
     <?php
     date_default_timezone_set("Asia/Ho_Chi_Minh");
     $current = date("H:ia");
-    $today = date('d/m/Y');
+    $today = date('Y/m/d');
     ?>
     @foreach($lsBook as $book)
     <?php
@@ -49,7 +49,7 @@
     ?>
     <div class="col-md-4 col-sm-4">
       <div class="jumbotron">
-        @if(date('d/m/Y', strtotime($book->book_date)) > $today && !$book->deleted_at)
+        @if(date('Y/m/d', strtotime($book->book_date)) < $today && !$book->deleted_at)
         <h1 class="display-4 text-secondary" style="font-size: 2rem;">Đơn đặt phòng <span class="text-danger">{{$book->id}}</span></h1>
         @elseif($book->deleted_at)
         <h1 class="display-4 text-danger" style="font-size: 2rem;">Đơn đặt phòng <span class="text-danger">{{$book->id}}</span></h1>
